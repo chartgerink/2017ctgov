eec0a47d26da4cdc8cdae0a2d864f4e5c1bacdf0
CHJ Hartgerink <chjh@protonmail.com>
2016-05-03 20:06:41 +0200

    Update xml parsing

--- functions/xml_parsing.R
+++ functions/xml_parsing.R
@@ -5,20 +5,33 @@ folder_files <- 'ctgov_raw_xml_20160414'
 
 files <- list.files(recursive = TRUE)
 sel <- files[grepl(pattern = folder_files, x = files)]
+q <- NULL
+
+for (i in 1:length(sel))
+# for (i in 1:5)
+{
+  # Initial xml parse
+  x <- XML::xmlTreeParse(sel[i])
+  # Get the root of the xml tree
+  y <- xmlRoot(x)
+  # Get the XML values
+  z <- xmlSApply(y, function(x) xmlSApply(x, xmlValue))
+  # Get the names from the xml object
+  q <- c(q, names(z))
+  
+  print(i)
+}
+
+write.table(q, file = 'data/names_trials', col.names = FALSE, row.names = FALSE)
+
+read.table('data/names_trials', header = FALSE)
+
+file <- x$doc$file
+xml_version <- x$doc$version
+
 
 dtdFile <- readLines('materials/public.dtd')
 
 XML::parseDTD(dtdFile, asText = TRUE)
 
 XML::xmlTreeParse(sel[1])
-
-x <- XML::xmlTreeParse(sel[1])
-
-y <- xmlRoot(x)
-
-z <- xmlSApply(y, function(x) xmlSApply(x, xmlValue))
-
-q <- data.frame(t(z), row.names = NULL)
-
-file <- x$doc$file
-xml_version <- x$doc$version
